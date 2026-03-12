1d029dde5636a4536bbcad35ae4785657229a194
susmithametta9-debug <susmithametta9@gmail.com>
2026-03-12 12:02:08 -0400

    Add ImagedisplayFragment to MainActivity with arguments

--- app/src/main/java/edu/temple/inclassactivity/ImageDisplayFragment.kt
+++ app/src/main/java/edu/temple/inclassactivity/ImageDisplayFragment.kt
@@ -1,7 +1,3 @@
-/**
- * Please study this class, but do not modify it
- */
-
 package edu.temple.inclassactivity
 
 import android.os.Bundle
@@ -20,25 +16,19 @@ class ImageDisplayFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // If we have arguments
-        arguments?.let { it ->
-            // If we find the specific argument
-            it.getIntArray(IMAGES_KEY)?.let {
+        arguments?.let { bundle ->
+            bundle.getIntArray(IMAGES_KEY)?.let {
                 images = it
             }
         }
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // The inflated layout file is returned to the parent/host and displayed to the user
         return inflater.inflate(R.layout.fragment_image_display, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        // The recycler view is the root element of the Fragment's layout
-        // as such the view argument passed to onViewCreated() is the RecyclerView
         with (view as RecyclerView) {
             adapter = CustomRecyclerAdapter(images)
             layoutManager = GridLayoutManager(requireContext(), 2)
--- app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
@@ -8,12 +8,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Fetch images into IntArray called imageArray
         val typedArray = resources.obtainTypedArray(R.array.image_ids)
         val imageArray = IntArray(typedArray.length()) {typedArray.getResourceId(it, 0)}
         typedArray.recycle()
 
-        // Attach an instance of ImageDisplayFragment using factory method
-
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fragmentContainerView, ImageDisplayFragment.newInstance(imageArray))
+                .commit()
+        }
     }
 }
